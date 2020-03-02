13e4265a53dc26c9064c1b2d35cbd01c941fd23a
Christian Dibala Bell <ChristianDibalaBell@gmail.com>
2020-03-02 14:23:25 -0500

    Adjusts load testing start commands

---
+++ loadtesting/proxy.js
@@ -0,0 +1,24 @@
+import { check, sleep } from 'k6';
+import http from 'k6/http';
+
+const desiredRPS = 10000;
+const RPSperVU = 20;
+const VUsRequired = Math.round(desiredRPS/RPSperVU);
+
+const sleepTime = 0.05;
+export let options = {
+  vus: VUsRequired,
+  duration: '600s'
+}
+
+export default function() {
+  const event_id = Math.floor(Math.random() * 1000000) + 9000000;
+  const url = `http://localhost:3000/?event_id=${event_id}`;
+  let res = http.get(url);
+
+  check(res, {
+    "is status 200": (r) => r.status === 200
+  });
+
+  sleep(sleepTime);
+};
