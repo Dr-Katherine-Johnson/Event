b201c5fb2a7acb74ad6811201a32e0e044daa9f4
Christian Dibala Bell <ChristianDibalaBell@gmail.com>
2020-03-08 03:46:45 -0400

    [wip] Sets up docker swarm

---
+++ deploying/docker-engine-install.sh
@@ -0,0 +1,27 @@
+# SSH into the instance
+# ssh -i ~/aws/Administrator-key-pair-useast1.pem ec2-user@EC2_INSTANCE_URL
+
+# update
+# sudo yum update -y
+
+# install docker
+# sudo yum install -y docker
+
+# start the docker service
+# sudo service docker start
+
+# add ec2-user to the docker user group
+# sudo usermod -a -G docker ec2-user
+
+# log out and back in
+
+
+# docker swarm mode
+# https://docs.docker.com/engine/swarm/swarm-tutorial/
+# setting up ONE (1) host to have the docker manager node
+# docker swarm init --advertise-addr <MANAGER-IP>
+
+# adding a worker to the swarm (run this command on the OTHER boxes - hosts with nodes that will be workers)
+# see .env file
+
+# set up the routing mesh for the swarm
